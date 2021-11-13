0a0f17fb07e58a8afbcc12050218136145a4e7f1
Lucas Giménez <luquetti.5@hotmail.com>
2021-11-13 17:53:22 -0300

    Google Clone

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center p-10 mt-10 border-t dark:border-gray-700 border-gray-200">
+      <a
+        href="https://www.linkedin.com/in/lucasgim%C3%A9nez1054/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Giménez Lucas
+      </a>
+    </div>
+  );
+};
